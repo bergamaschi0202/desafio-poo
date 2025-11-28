835f84bc525698190a48f8c3002bf8a900936ae6
Arthur Bergamaschi Souza Sarria <bergamaschiarthur6@gmail.com>
2025-11-28 16:49:15 -0300

    primeiro commit

---
+++ src/br/com/arthur/desafiopoo/Main.java
@@ -0,0 +1,14 @@
+package br.com.arthur.desafiopoo;
+
+import br.com.arthur.desafiopoo.Entity.Curso;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+
+
+    }
+}
